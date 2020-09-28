399fac93129d09afb8ad94c752b8b5e345cfc3ac
win.cao <win@xxx.com>
2020-09-28 17:26:35 +0800

    set hwrev v11 by fix adc value

--- cmd/hwrev.c
+++ cmd/hwrev.c
@@ -14,10 +14,14 @@ int do_hwrev(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 {
 	unsigned int hwrev_adc;
 
+#if 0
 	if (adc_channel_single_shot("saradc", 0, &hwrev_adc)) {
 		printf("board hw rev failed\n");
 		return CMD_RET_FAILURE;
 	}
+#else
+    hwrev_adc = 676;
+#endif
 
 	/* rev 1.1 */
 	if (check_range(655, 695, hwrev_adc)) {
@@ -35,7 +39,7 @@ int do_hwrev(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 		env_set("dtb_name", "rk3326-odroidgo2-linux.dtb");
 	}
 
-	printf("adc0 (hw rev) %d\n", hwrev_adc);
+	printf("sd boot: adc0 (hw rev) %d\n", hwrev_adc);
 
 	return CMD_RET_SUCCESS;
 }
